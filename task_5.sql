428c0dfa7d0fb94651ea45fd7516c4ea06494d80
Irina Volnoboi <irinavolnoboy@gmail.com>
2024-10-30 18:12:03 +0300

    Complete task 5

--- task_5.sql
+++ task_5.sql
@@ -1,1 +1,9 @@
-/*добавьте сюда запрос для решения задания 5*/
+/*The highest and the lowest price by country.*/
+SELECT 
+    c.name AS brand_origin, 
+    MAX(s.price * 100 / (100 - discount)) AS price_max, 
+    MIN(s.price * 100 / (100 - discount)) AS price_min
+FROM car_shop.countries c 
+LEFT JOIN car_shop.autos a ON c.id = a.country_id 
+LEFT JOIN car_shop.sales s ON a.id = s.auto_id
+GROUP BY c.name;
