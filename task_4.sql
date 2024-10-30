7896dccc7f06a28112736f277597a038fdc4200a
Irina Volnoboi <irinavolnoboy@gmail.com>
2024-10-30 17:40:22 +0300

    Fix column name

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,5 @@
 /*List of purchased cars for each user.*/
-SELECT c.name, string_agg(a.brand_name || ' ' || a.model_name, ', ') 
+SELECT c.name, string_agg(a.brand_name || ' ' || a.model_name, ', ') AS cars
 FROM car_shop.clients c 
 LEFT JOIN car_shop.sales s ON c.id = s.client_id
 LEFT JOIN car_shop.autos a ON a.id = s.auto_id
