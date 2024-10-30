a4f9449f7dd4fa0ca35b7e6db52829a77124eaa5
Irina Volnoboi <irinavolnoboy@gmail.com>
2024-10-30 13:56:32 +0300

    Complete task 3

--- task_3.sql
+++ task_3.sql
@@ -1,1 +1,9 @@
-/*добавьте сюда запрос для решения задания 3*/
+/*Average price of all cars by month in 2022.*/
+SELECT
+    EXTRACT('Month' FROM s.date) AS month, 
+    2022 AS YEAR,
+    ROUND(AVG(s.price), 2) AS price_avg
+FROM car_shop.sales s
+WHERE EXTRACT('Year' FROM s.date) = 2022
+GROUP BY 1
+ORDER BY 1;
