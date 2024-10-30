e327e68c4aa5e5ecb1ed27ead755a70077afb656
Irina Volnoboi <irinavolnoboy@gmail.com>
2024-10-30 13:21:34 +0300

    Add initial queries

--- INITIAL.sql
+++ INITIAL.sql
@@ -1,1 +1,16 @@
-/*сохраните в этом файле запросы для первоначальной загрузки данных - создание схемы raw_data и таблицы sales и наполнение их данными из csv файла*/
+/*Queries for creating and populating the table with raw data from the csv file*/
+CREATE SCHEMA raw_data;
+
+CREATE TABLE raw_data.sales (
+    id SMALLINT PRIMARY KEY,
+    auto VARCHAR,
+    gasoline_consumption NUMERIC,
+    price NUMERIC,
+    date DATE,
+    person_name VARCHAR,
+    phone VARCHAR,
+    discount SMALLINT,
+    brand_origin VARCHAR
+);
+
+\copy raw_data.sales(id, auto, gasoline_consumption, price, date, person_name, phone, discount, brand_origin) FROM 'C:\Temp\cars.csv' CSV HEADER NULL 'null';
