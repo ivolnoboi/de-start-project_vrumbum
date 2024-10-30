2e06ebcc63c9119d2f8548569d7cec18f4da4123
Irina Volnoboi <irinavolnoboy@gmail.com>
2024-10-30 17:37:11 +0300

    Complete task 4

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,7 @@
-/*добавьте сюда запрос для решения задания 4*/
+/*List of purchased cars for each user.*/
+SELECT c.name, string_agg(a.brand_name || ' ' || a.model_name, ', ') 
+FROM car_shop.clients c 
+LEFT JOIN car_shop.sales s ON c.id = s.client_id
+LEFT JOIN car_shop.autos a ON a.id = s.auto_id
+GROUP BY c.name
+ORDER BY c.name;
