edc46456d3a76f1c0ce83c5916d1a7686ad0d9fa
Irina Volnoboi <irinavolnoboy@gmail.com>
2024-10-30 13:41:19 +0300

    Complete task 2

--- task_2.sql
+++ task_2.sql
@@ -1,1 +1,9 @@
-/*добавьте сюда запрос для решения задания 2*/
+/*Average price of cars by year.*/
+SELECT 
+    a.brand_name, 
+    EXTRACT('Year' FROM s.date) AS year, 
+    ROUND(AVG(s.price), 2) AS price_avg
+FROM car_shop.sales s
+JOIN car_shop.autos a ON s.auto_id = a.id
+GROUP BY a.brand_name, EXTRACT('Year' FROM s.date)
+ORDER BY a.brand_name, EXTRACT('Year' FROM s.date);
