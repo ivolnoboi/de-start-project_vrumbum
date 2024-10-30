6286f59ce8ede931375763a5c1a1c5f6c915a2d8
Irina Volnoboi <irinavolnoboy@gmail.com>
2024-10-30 13:40:26 +0300

    Complete task 1

--- task_1.sql
+++ task_1.sql
@@ -1,1 +1,3 @@
-/*добавьте сюда запрос для решения задания 1*/
+/*Percentage of electric vehicles in the table.*/
+SELECT (1 - count(gasoline_consumption)::real / count(*)) * 100 AS nulls_percentage_gasoline_consumption
+FROM car_shop.autos;
